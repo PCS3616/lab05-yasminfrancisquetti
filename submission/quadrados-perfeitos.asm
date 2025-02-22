fbd788f34bd83d2d90eee187fd8e6edf14fad070
yasminfrancisquetti <yasminfrancisquetti@usp.br>
2025-02-22 19:40:51 +0000

    Quarto envio

--- submission/quadrados-perfeitos.asm
+++ submission/quadrados-perfeitos.asm
@@ -6,9 +6,12 @@ START	K /0000
 LOOP1	LD N 		; iniciar loop - colocar N no acumulador
 	SB SSC		; subtrair 65 do acumulador
 	JZ END		; jump pra fora do loop if zero
+	LD N
+	MM n		
 LOOP2	LD n		; iniciar mais um loop - colocar n no acumulador
 	JZ END2
 	SB UM		; subtrair 1 do acumulador
+	MM n		; colocar o acumulador em n
 	JZ END2		; jump pra fora do loop if zero
 	ML DOIS		; multiplicar o acumulador por 2
 	SB UM		; subtrair 1 do acumulador
@@ -16,8 +19,11 @@ LOOP2	LD n		; iniciar mais um loop - colocar n no acumulador
 	MM QP		; colocar o acumulador em QP
 	JP LOOP2	; voltar pro inicio do loop
 END2	LD QP
-	MM M		; colocar QP no endereço M da memoria
+	MM M		; colocar QP no endereço M da memoria // aqui esta o erro
 	LD M		; colocar M no acumulador
+	AD MMOPCODE
+	MM EXECMM
+	EXECMM K /0000
 	AD DOIS		; somar 2 no acumulador
 	MM M		; colocar o acumulador no M
 	LD N		
@@ -34,3 +40,4 @@ UM 	K /0001 ; 1
 DOIS 	K /0002 ; 2
 QP	K /0000 ; quadrado perfeito
 n	K /0000 ; contador do loop
+MMOPCODE 	K /8000
